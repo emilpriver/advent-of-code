e629cf1010dfd6a9f63df934719fe94efc75ff1e
Emil Privér <emil@priver.se>
2022-12-03 15:44:23 +0100

    not finished solution

--- 2022/Day3/Part2/src/main.rs
+++ 2022/Day3/Part2/src/main.rs
@@ -6,25 +6,25 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let contents_array = contents.lines().chunks(3);
+    let contents_array = contents.lines().into_iter().chunks(3);
 
     let mut equal_chars: Vec<char> = vec![];
     let mut total_points: usize = 0;
 
     for line in &contents_array {
-        let line_into_vec = line.collect_vec();
-        let [first_line, second_line, third_line] = line_into_vec[0..2];
+        let line_into_vec = line.into_iter();
+        let [first_line, second_line, third_line] = line_into_vec[0..3]  else { panic!("rip") };
         let first_part_array: Vec<char> = first_line.chars().into_iter().unique().collect();
         let second_part_array: Vec<char> = second_line.chars().into_iter().unique().collect();
-        let third_part_array: Vec<char> = third_line.chars().into_iter().unique().collect();
+        let mut third_part_array: Vec<char> = third_line.chars().into_iter().unique().collect();
 
-        for x in first_part_array {
-            for y in second_part_array {
-                if third_part_array.contains(&x) && third_part_array.contains(&y) {
-                    equal_chars.push(x);
+        for x in &first_part_array {
+            for y in &second_part_array {
+                if third_part_array.contains(x) && third_part_array.contains(y) {
+                    equal_chars.push(*x);
 
                     // we need to remove the char from the array
-                    third_part_array.remove(third_part_array.iter().position(|&r| r == x).unwrap());
+                    third_part_array.remove(third_part_array.iter().position(|&r| r == *x).unwrap());
                 }
             }
         }
