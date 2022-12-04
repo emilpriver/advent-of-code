e756e91403a5f86da62ce0d84b42cd4b988c5879
Emil Privér <emil@priver.se>
2022-12-04 11:06:53 +0100

    dev

--- 2022/Day3/Part2/src/main.rs
+++ 2022/Day3/Part2/src/main.rs
@@ -1,22 +1,25 @@
-use itertools::Itertools;
 use std::fs;
+use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
 
 fn main() {
     let file_path = "./input.txt".to_string();
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let contents_array = contents.lines().into_iter().chunks(3);
-
     let mut equal_chars: Vec<char> = vec![];
     let mut total_points: usize = 0;
 
-    for line in &contents_array {
-        let line_into_vec = line.into_iter();
-        let [first_line, second_line, third_line] = line_into_vec[0..3]  else { panic!("rip") };
-        let first_part_array: Vec<char> = first_line.chars().into_iter().unique().collect();
-        let second_part_array: Vec<char> = second_line.chars().into_iter().unique().collect();
-        let mut third_part_array: Vec<char> = third_line.chars().into_iter().unique().collect();
+    let lines: Vec<&str> = contents.trim().split('\n').collect();
+
+    lines.chunks(3).map(|chunk| {
+        let [first_line, second_line, third_line] = chunk;
+        let first_part_array: Vec<char> =
+            first_line.chars().into_iter().unique().collect();
+        let second_part_array: Vec<char> =
+            second_line.chars().into_iter().unique().collect();
+        let mut third_part_array: Vec<char> =
+            third_line.chars().into_iter().unique().collect();
 
         for x in &first_part_array {
             for y in &second_part_array {
@@ -24,11 +27,12 @@ fn main() {
                     equal_chars.push(*x);
 
                     // we need to remove the char from the array
-                    third_part_array.remove(third_part_array.iter().position(|&r| r == *x).unwrap());
+                    third_part_array
+                        .remove(third_part_array.iter().position(|&r| r == *x).unwrap());
                 }
             }
         }
-    }
+    });
 
     for char in equal_chars {
         let ascii: usize = char as usize;
