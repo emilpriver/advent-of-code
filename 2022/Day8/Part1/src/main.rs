0e8faa7c77d8e3159d4dc95114bc4dad863dfe0c
Emil Privér <emil@priver.se>
2022-12-08 11:09:20 +0100

    dev

--- 2022/Day8/Part1/src/main.rs
+++ 2022/Day8/Part1/src/main.rs
@@ -11,21 +11,20 @@ fn get_vertical_trees(input: String, index: usize) -> Vec<i64> {
         .collect::<Vec<i64>>()
 }
 
-fn count_visible_tree(input: String) -> usize {
-    let lines = input.split('\n').collect::<Vec<&str>>();
+fn count_visible_tree(input: String) -> i64 {
+    let lines = input.lines().collect::<Vec<&str>>();
     let total_lines = lines.len();
 
     // we want to add the vertical edge lines total trees to the variable
-    let mut total_visible_trees = total_lines * 2;
+    let mut total_visible_trees: i64 = (total_lines * 2).to_string().parse::<i64>().unwrap();
 
     for (index, line) in lines.into_iter().enumerate() {
         if index == 0 || index == (total_lines - 1) {
-            total_visible_trees += line.len();
             // all of these trees are already visible
+            total_visible_trees += line.len().to_string().parse::<i64>().unwrap();
             continue;
         }
 
-        let mut visible_trees = 0;
         let line_to_chars: Vec<i64> = line
             .chars()
             .map(|x| x.to_string().parse::<i64>().unwrap())
@@ -33,33 +32,42 @@ fn count_visible_tree(input: String) -> usize {
 
         let line_as_iter = line_to_chars.clone().into_iter().enumerate();
 
+        println!();
+        println!("h {:?}", index);
         for (char_index, char) in line_as_iter {
+            let char_as_int = char.to_string().parse::<i64>().unwrap();
+
             if char_index == 0 || char_index == (line_to_chars.len() - 1) {
-                // the first char and the last is already calculated
+                // the first and last line is already visible
+                continue;
+            }
+            println!("v {:?}", char_index);
+
+            let horizontal_split = line_to_chars.split_at(char_index);
+            if horizontal_split.0.iter().all(|x| x < &char_as_int)
+                || horizontal_split.1[1..].iter().all(|x| x < &char_as_int)
+            {
+                // tree is visible, lets add it
+                total_visible_trees += 1;
+                println!("{:?}", char);
                 continue;
             }
 
-            let horizontal_split = line_to_chars.split_at(char_index);
-            if horizontal_split.0.iter().all(|x| x < &char)
-                || horizontal_split.1.iter().all(|x| x < &char)
+            let vertical_tree = get_vertical_trees(input.clone(), char_index);
+            let vertical_split = vertical_tree.split_at(char_index);
+            println!("{:?}", vertical_split);
+            if vertical_split.0.iter().all(|x| x < &char_as_int)
+                || vertical_split.1[1..].iter().all(|x| x < &char_as_int)
             {
                 // tree is visible, lets add it
-                visible_trees += 1;
-            } else {
-                let vertical_tree = get_vertical_trees(input.clone(), char_index);
-                let vertical_split = line_to_chars.split_at(char_index);
-                if vertical_split.0.iter().all(|x| x < &char)
-                    || vertical_split.1.iter().all(|x| x < &char)
-                {
-                    visible_trees += 1;
-                }
+                println!("{:?}", char);
+
+                total_visible_trees += 1;
             }
         }
-
-        total_visible_trees += visible_trees
     }
 
-    total_visible_trees
+    total_visible_trees - 4
 }
 
 fn main() {
